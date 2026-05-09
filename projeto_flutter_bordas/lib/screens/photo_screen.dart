4e66ef85ac997e5fa87273991d6f59bfaeb8044d
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2026-05-09 16:21:56 -0300

    ✨ feat: add edge detection screen with grayscale and border processing

--- projeto_flutter_bordas/lib/screens/photo_screen.dart
+++ projeto_flutter_bordas/lib/screens/photo_screen.dart
@@ -1,16 +1,85 @@
+import 'dart:typed_data';
 import 'dart:io';
+import 'package:image/image.dart' as img;
+
 import 'package:flutter/material.dart';
 
-class PhotoScreen extends StatelessWidget {                                                                                                             
+class PhotoScreen extends StatefulWidget {                                                                                                              
   final String path;
                                                                                                                                                         
   const PhotoScreen({super.key, required this.path});
 
   @override
+  State<PhotoScreen> createState() => _PhotoScreenState();
+}
+
+class _PhotoScreenState extends State<PhotoScreen> {
+
+  Uint8List? _processedImage; 
+  Uint8List? _grayImage;                                                                                                                            
+  bool _isProcessing = true;      
+
+  @override                                                                                                                                               
+  void initState() {                                                                                                                                      
+    super.initState();                                                                                                                                    
+    _processImage();                                                                                                                                      
+  }               
+
+  Future<void> _processImage() async {
+    
+    final bytes = await File(widget.path).readAsBytes();
+
+    final original = img.decodeImage(bytes)!;
+
+    final gray = img.grayscale(original);
+
+    final grayEncoded = img.encodePng(gray);
+
+    final edgeImage = img.Image(width: gray.width, height: gray.height);   
+
+    for (int y = 1; y < gray.height - 1; y++) {                                                                                                             
+      for (int x = 1; x < gray.width - 1; x++) {                                                                                                            
+        final p = gray.getPixel(x, y).r.toInt();
+        final right = gray.getPixel(x + 1, y).r.toInt();
+        final bottom = gray.getPixel(x, y + 1).r.toInt();
+        final diff = ((p - right).abs() + (p - bottom).abs()).clamp(0, 255);
+        edgeImage.setPixelRgb(x, y, diff, diff, diff);
+      }
+    }
+
+      final encoded = img.encodePng(edgeImage);
+      setState(() {
+        _grayImage = grayEncoded;
+        _processedImage = encoded;
+        _isProcessing = false;
+      });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Image.file(File(path)),
-      appBar: AppBar(title: const Text('Foto'),),
-    );
+    if (_isProcessing) {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+
+    } else { 
+    return Scaffold(                                                                                                                                        
+        appBar: AppBar(title: const Text('Resultado')),                                                                                                       
+        body: SingleChildScrollView(                                                                                                                          
+          child: Column(                                                                                                                                      
+            children: [
+              const Text('Original'),
+              Image.file(File(widget.path)),
+              const Text('Tons de cinza'),
+              Image.memory(_grayImage!),
+              const Text('Bordas detectadas'),
+              Image.memory(_processedImage!),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
